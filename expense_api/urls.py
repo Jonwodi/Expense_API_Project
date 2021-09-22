814a8c0b9b2455e15565a392b4212a004820dc44
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2021-09-22 18:04:58 +0100

    Implement retrieve single expense

--- expense_api/urls.py
+++ expense_api/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("expenses/", views.ExpenseListCreateView.as_view(), name="expense-list-create")
-]  # urls path
+    path(
+        "expenses/", views.ExpenseListCreateView.as_view(), name="expense-list-create"
+    ),
+    path(
+        "expenses/<str:pk>",
+        views.ExpenseRetrieveUpdateDestroyView.as_view(),
+        name="expense-retrieve-update-destroy",
+    ),
+]
+# urls path
